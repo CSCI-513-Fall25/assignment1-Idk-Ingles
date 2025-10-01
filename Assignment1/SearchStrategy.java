03bf9965c33a672862e479624082436353eaff34
pranavkocherlakota <pranavsharma9481@gmail.com>
2025-10-01 12:35:27 -0500

    Delete Assignment1/SearchStrategy.java
    Deleting to organize it properly

--- Assignment1/SearchStrategy.java
+++
@@ -1,9 +0,0 @@
-package test; // base interface for all strategies
-
-public interface SearchStrategy {
-	// search the grid for 8 ship coordinates
-	public int[][] search(int[][] grid, int[][] coordinates); 
-
-	// return how many cells were searched in process
-	public int getCellCount(); 
-}
